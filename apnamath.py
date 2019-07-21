da64b3efb154127d679bd03da414f02587985925
shahnihal09 <nihal.gaditek@gmail.com>
2019-07-21 20:49:17 +0500

    21julyclass

---
+++ apnamath.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(a,b):
+    print(a+b)
+
+
+# In[2]:
+
+
+def sub(a,b):
+    print(a-b)
+
+
+# In[3]:
+
+
+def mul(a,b):
+    print(a*b)
+
+
+# In[4]:
+
+
+def div(a,b):
+    print(a/b)
+
+
+# In[5]:
+
+
+def mynewmodulo(a,b):
+    print(a%b)
+
+
+# In[ ]:
+
+
+
+
